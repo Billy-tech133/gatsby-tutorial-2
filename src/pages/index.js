4f8e671dcf6128e6adde3435b95a0ac7f39654c3
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-09-27 08:45:32 +0100

    making progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
-
+import { Link } from "gatsby"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <div>
+        <Link to="/about">about</Link>
+      </div>
+      <a href="about">reg link</a>
+      <a href="http://www.gatsbyjs.com/docs/">gatsby docs</a>
+    </div>
+  )
 }
